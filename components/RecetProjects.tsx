bf9670a32a50d5238c0e0fd9db575fa22a5ecd9d
PeicoPaco <rodrigo.reategui.g@gmail.com>
2024-06-26 17:57:38 -0500

    made some changes to the page according to experience

--- components/RecetProjects.tsx
+++ components/RecetProjects.tsx
@@ -23,16 +23,11 @@ const RecentProjects = () => {
               href="https://twitter.com/mannupaaji"
             >
               <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
-                <div
-                  className="relative w-full h-full overflow-hidden lg:rounded-3xl"
-                  style={{ backgroundColor: "#13162D" }}
-                >
-                  <img src="/bg.png" alt="bgimg" />
-                </div>
+                
                 <img
                   src={item.img}
-                  alt="cover"
-                  className="z-10 absolute bottom-0"
+                  alt={item.img}
+                  className="object-cover object-center w-full h-full"
                 />
               </div>
 
